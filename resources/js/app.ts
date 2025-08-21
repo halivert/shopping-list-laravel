772caaa33bcdac33234af732d77b8738698a034b
Halí V. <halivelazquez@outlook.com>
2025-08-20 22:22:01 -0600

    feat: add reverb

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -1,40 +1,56 @@
-import '../css/app.css';
+import "../css/app.css"
 
-import { createInertiaApp } from '@inertiajs/vue3';
-import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
-import type { DefineComponent } from 'vue';
-import { createApp, h } from 'vue';
-import { ZiggyVue } from 'ziggy-js';
-import { initializeTheme } from './composables/useAppearance';
+import { createInertiaApp } from "@inertiajs/vue3"
+import { configureEcho } from "@laravel/echo-vue"
+import { resolvePageComponent } from "laravel-vite-plugin/inertia-helpers"
+import type { DefineComponent } from "vue"
+import { createApp, h } from "vue"
+import { ZiggyVue } from "ziggy-js"
+import { initializeTheme } from "./composables/useAppearance"
 
 // Extend ImportMeta interface for Vite...
-declare module 'vite/client' {
-    interface ImportMetaEnv {
-        readonly VITE_APP_NAME: string;
-        [key: string]: string | boolean | undefined;
-    }
+// declare module 'vite/client' {
+//     interface ImportMetaEnv {
+//         readonly VITE_APP_NAME: string;
+//         [key: string]: string | boolean | undefined;
+//     }
+//
+//     interface ImportMeta {
+//         readonly env: ImportMetaEnv;
+//         readonly glob: <T>(pattern: string) => Record<string, () => Promise<T>>;
+//     }
+// }
+//
 
-    interface ImportMeta {
-        readonly env: ImportMetaEnv;
-        readonly glob: <T>(pattern: string) => Record<string, () => Promise<T>>;
-    }
-}
+configureEcho({
+    broadcaster: "reverb",
+    // key: import.meta.env.VITE_REVERB_APP_KEY,
+    // wsHost: import.meta.env.VITE_REVERB_HOST,
+    // wsPort: import.meta.env.VITE_REVERB_PORT,
+    // wssPort: import.meta.env.VITE_REVERB_PORT,
+    // forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? 'https') === 'https',
+    // enabledTransports: ['ws', 'wss'],
+})
 
-const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+const appName = import.meta.env.VITE_APP_NAME || "Laravel"
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
-    resolve: (name) => resolvePageComponent(`./pages/${name}.vue`, import.meta.glob<DefineComponent>('./pages/**/*.vue')),
+    resolve: (name) =>
+        resolvePageComponent(
+            `./pages/${name}.vue`,
+            import.meta.glob<DefineComponent>("./pages/**/*.vue")
+        ),
     setup({ el, App, props, plugin }) {
         createApp({ render: () => h(App, props) })
             .use(plugin)
             .use(ZiggyVue)
-            .mount(el);
+            .mount(el)
     },
     progress: {
-        color: '#4B5563',
+        color: "#4B5563",
     },
-});
+})
 
 // This will set light / dark mode on page load...
-initializeTheme();
+initializeTheme()
